b14e05cd8274ac0ec359e21ec941f303d2759607
Jeff Manian <jeffrey.manian@gmail.com>
2018-01-20 17:51:00 -0500

    echo text in DM

--- events.py
+++ events.py
@@ -2,35 +2,45 @@ import json
 import logging
 import os
 # import re
-
-expected_token = os.environ['VERIFICATION_TOKEN']
+from slackclient import SlackClient
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+expected_token = os.environ['VERIFICATION_TOKEN']
+sc = SlackClient(os.environ['BOT_ACCESS_TOKEN'])
 
 def lambda_handler(event, context):
     logger.info("Event received: %s", event)
     body = json.loads(event['body'])
 
     if body['token'] != expected_token:
-        return {
-            'statusCode': 401,
-            'body': None
-        }
+        return empty_response(401)
 
     if body['type'] == 'url_verification':
-        return {
-            'statusCode': 200,
-            'body': body['challenge'],
-            'headers': {
-                'Content-Type': 'application/json'
-            },
+        return verification_response(body)
+
+    channel = body['event']['channel']
+    if channel[0] == 'D'
+        text = body['event']['text']
+        sc.api_call("chat.postMessage", channel=channel, text=text)
+
+    return empty_response(200)
+
+def empty_response(code):
+    return {
+        'statusCode': code,
+        'body': None,
+        'headers': {
+            'Content-Type': 'application/json'
         }
-    else:
-        return {
-            'statusCode': 200,
-            'body': None,
-            'headers': {
-                'Content-Type': 'application/json'
-            }
-        }
+    }
+
+def verification_response(body):
+    return {
+        'statusCode': 200,
+        'body': body['challenge'],
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+    }
