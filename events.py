fce1ee623c677e28f5a9654ba76beb5eda4730d9
Jeff Manian <jeffrey.manian@gmail.com>
2018-02-12 23:39:29 -0500

    don't respond to slackbot, and some refactoring

--- events.py
+++ events.py
@@ -22,39 +22,41 @@ def lambda_handler(event, context):
     if body['type'] == 'url_verification':
         return verification_response(body)
 
-
     process_event(body['event'])
-
     return empty_response(200)
 
 def process_event(event):
     if event['type'] == 'message':
-        # don't respond to self
+        # don't respond to self or slackbot
         if 'bot_id' in event and event['bot_id'] == bot_id:
             return
         if 'user' in event and event['user'] == user_id:
             return
+        if 'user' in event and event['user'] == 'USLACKBOT':
+            return
 
         if 'text' in event:
             channel = event['channel']
-            text = event['text']
             thread_ts = event.get('thread_ts')
+            text = event['text']
+            respond_to_text(channel, thread_ts, text)
 
-            # Grill Vogel
-            if re.search(r"\bgreat(er|est)?\b", text, re.I) != None:
-                word = "hug"
-                match = re.search(r"\bgreat(?:er|est)?\b (\S+)", text, re.I)
-                if match:
-                    word = match.group(1)
-                post_text = "Ah, the greatest %s!" % word
-                username = "Grill Vogel"
-                post_message(channel, thread_ts, post_text, username, "grill_vogel.jpg")
+def respond_to_text(channel, thread_ts, text):
+    # Grill Vogel
+    if re.search(r"\bgreat(er|est)?\b", text, re.I) != None:
+        word = "hug"
+        match = re.search(r"\bgreat(?:er|est)?\b (\S+)", text, re.I)
+        if match:
+            word = match.group(1)
+        post_text = "Ah, the greatest %s!" % word
+        username = "Grill Vogel"
+        post_message(channel, thread_ts, post_text, username, "grill_vogel.jpg")
 
-            # Peach
-            if re.search(r"\bbingo\b", text, re.I) != None:
-                post_text = "Bingo, bye-bye!"
-                username = 'Peach'
-                post_message(channel, thread_ts, post_text, username, "peach.png")
+    # Peach
+    if re.search(r"\bbingo\b", text, re.I) != None:
+        post_text = "Bingo, bye-bye!"
+        username = 'Peach'
+        post_message(channel, thread_ts, post_text, username, "peach.png")
 
 def post_message(channel, thread_ts, text, username, icon_name):
     if thread_ts == None:
@@ -70,8 +72,8 @@ def post_message(channel, thread_ts, text, username, icon_name):
         sc.api_call(
             "chat.postMessage",
             channel=channel,
-            text=text,
             thread_ts=thread_ts,
+            text=text,
             as_user=False,
             username=username,
             icon_url=icon_url(icon_name)
