4ac59b067e6de9faf7863412a93fcc7277cc32fa
Jeff Manian <jeffrey.manian@gmail.com>
2018-03-07 01:14:57 -0500

    update build script for chaos and more packages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # demon
 
 Run `build.sh` to build the python files and dependencies into `build.zip`, which can be uploaded to AWS Lambda.
+
+`git submodule update --remote` to update the submodule, and then commit the changes.
--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 mkdir build
 cp *.py build
+mkdir build/chaos
+cp -R chaos build/chaos
+
 pip install slackclient -U -t build
+pip install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl -U -t build
+pip install torchvision -U -t build
 cd build
 zip -r ../build .
 cd ..
